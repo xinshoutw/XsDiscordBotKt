ea85a4e2ad9ef812ef7e37820f6df993ccb60c30
xinshoutw <icexinshou@gmail.com>
2025-02-04 17:14:45 +0800

    refactor: remove unnecessary indents

--- Plugins/Ticket/src/main/kotlin/tw/xserver/plugin/ticket/create/StepManager.kt
+++ Plugins/Ticket/src/main/kotlin/tw/xserver/plugin/ticket/create/StepManager.kt
@@ -73,7 +73,8 @@ internal object StepManager {
 
 
     private fun previewReason(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
+        val step = steps[event.user.idLong] ?: return
+        event.deferEdit().flatMap {
             event.replyModal(
                 modalCreator.getModalBuilder(
                     "preview-reason",
@@ -82,236 +83,238 @@ internal object StepManager {
                         "tt@reason-title", step.data.reasonTitle
                     )
                 ).build()
-            ).queue()
-        }
+            )
+        }.queue()
     }
 
     private fun authorForm(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            event.replyModal(
-                modalCreator.getModalBuilder(
-                    "author-form",
-                    event.userLocale,
-                    substitutor = Placeholder.getSubstitutor(event).putAll(
-                        "tt@author" to (step.data.author ?: ""),
-                        "tt@image" to (step.data.authorIconUrl ?: "")
-                    )
-                ).build()
-            ).queue()
-        }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        event.replyModal(
+            modalCreator.getModalBuilder(
+                "author-form",
+                event.userLocale,
+                substitutor = Placeholder.getSubstitutor(event).putAll(
+                    "tt@author" to (step.data.author ?: ""),
+                    "tt@image" to (step.data.authorIconUrl ?: "")
+                )
+            ).build()
+        ).queue()
     }
 
     private fun content(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            event.replyModal(
-                modalCreator.getModalBuilder(
-                    "content-form",
-                    event.userLocale,
-                    substitutor = Placeholder.getSubstitutor(event).putAll(
-                        "tt@title" to (step.data.title ?: ""),
-                        "tt@description" to (step.data.description ?: "")
-                    )
-                ).build()
-            ).queue()
-        }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        event.replyModal(
+            modalCreator.getModalBuilder(
+                "content-form",
+                event.userLocale,
+                substitutor = Placeholder.getSubstitutor(event).putAll(
+                    "tt@title" to (step.data.title ?: ""),
+                    "tt@description" to (step.data.description ?: "")
+                )
+            ).build()
+        ).queue()
     }
 
     private fun categoryMenu(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            event.deferEdit().flatMap {
-                step.hook.editOriginal(
-                    messageCreator.getEditBuilder(
-                        "modify-category",
-                        event.userLocale,
-                        modelMapper = mapOf(
-                            "tt@embed-demo" to step.previewEmbed,
-                            "tt@btn-demo" to step.previewComponent,
-                        )
-                    ).build()
-                )
-            }.queue()
-        }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        event.deferEdit().flatMap {
+            step.hook.editOriginal(
+                messageCreator.getEditBuilder(
+                    "modify-category",
+                    event.userLocale,
+                    modelMapper = mapOf(
+                        "tt@embed-demo" to step.previewEmbed,
+                        "tt@btn-demo" to step.previewComponent,
+                    )
+                ).build()
+            )
+        }.queue()
     }
 
     private fun colorForm(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            event.replyModal(
-                modalCreator.getModalBuilder(
-                    "modify-embed-color",
-                    event.userLocale,
-                    substitutor = Placeholder.getSubstitutor(event).put(
-                        "tt@hex-color", String.format("#%06X", step.data.color and 0xFFFFFF)
-                    )
-                ).build()
-            ).queue()
-        }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        event.replyModal(
+            modalCreator.getModalBuilder(
+                "modify-embed-color",
+                event.userLocale,
+                substitutor = Placeholder.getSubstitutor(event).put(
+                    "tt@hex-color", String.format("#%06X", step.data.color and 0xFFFFFF)
+                )
+            ).build()
+        ).queue()
     }
 
     private fun btnTextForm(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            event.replyModal(
-                modalCreator.getModalBuilder(
-                    "modify-btn-text",
-                    event.userLocale,
-                    substitutor = Placeholder.getSubstitutor(event).putAll(
-                        "tt@btn-text" to (step.data.btnText ?: ""),
-                        "tt@btn-emoji" to (step.data.btnEmoji?.asReactionCode ?: "")
-                    )
-                ).build()
-            ).queue()
-        }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        event.replyModal(
+            modalCreator.getModalBuilder(
+                "modify-btn-text",
+                event.userLocale,
+                substitutor = Placeholder.getSubstitutor(event).putAll(
+                    "tt@btn-text" to (step.data.btnText ?: ""),
+                    "tt@btn-emoji" to (step.data.btnEmoji?.asReactionCode ?: "")
+                )
+            ).build()
+        ).queue()
     }
 
     private fun btnColorMenu(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            event.deferEdit().flatMap {
-                it.editOriginal(
-                    messageCreator.getEditBuilder(
-                        "modify-btn-color",
-                        event.userLocale,
-                        modelMapper = mapOf(
-                            "tt@embed-demo" to step.previewEmbed,
-                            "tt@btn-demo" to step.previewComponent,
-                        )
-                    ).build()
-                )
-            }.queue()
-        }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        event.deferEdit().flatMap {
+            it.editOriginal(
+                messageCreator.getEditBuilder(
+                    "modify-btn-color",
+                    event.userLocale,
+                    modelMapper = mapOf(
+                        "tt@embed-demo" to step.previewEmbed,
+                        "tt@btn-demo" to step.previewComponent,
+                    )
+                ).build()
+            )
+        }.queue()
     }
 
     private fun reasonForm(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            event.replyModal(
-                modalCreator.getModalBuilder(
-                    "reason-form",
-                    event.userLocale,
-                    modelMapper = mapOf(
-                        "tt@reason-title" to step.data.reasonTitle
-                    )
-                ).build()
-            ).queue()
-        }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        event.replyModal(
+            modalCreator.getModalBuilder(
+                "reason-form",
+                event.userLocale,
+                modelMapper = mapOf(
+                    "tt@reason-title" to step.data.reasonTitle
+                )
+            ).build()
+        ).queue()
     }
 
     private fun adminMenu(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            event.deferEdit().flatMap {
-                step.hook.editOriginal(
-                    messageCreator.getEditBuilder(
-                        "modify-admin-role",
-                        event.userLocale,
-                        modelMapper = mapOf(
-                            "tt@embed-demo" to step.previewEmbed,
-                            "tt@btn-demo" to step.previewComponent,
-                        )
-                    ).build()
-                )
-            }.queue()
-        }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        event.deferEdit().flatMap {
+            step.hook.editOriginal(
+                messageCreator.getEditBuilder(
+                    "modify-admin-role",
+                    event.userLocale,
+                    modelMapper = mapOf(
+                        "tt@embed-demo" to step.previewEmbed,
+                        "tt@btn-demo" to step.previewComponent,
+                    )
+                ).build()
+            )
+        }.queue()
     }
 
     private fun confirmCreate(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            val manager = jsonGuildManager[event.guild!!.idLong]
-            step.confirmCreateAction(event.userLocale, event.channel).map {
-                manager.computeIfAbsent(it.id, JsonArray()).asJsonArray.add(step.json)
-                manager.save()
-            }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        val manager = jsonGuildManager[event.guild!!.idLong]
+        step.confirmCreateAction(event.userLocale, event.channel).map {
+            manager.computeIfAbsent(it.id, JsonArray()).asJsonArray.add(step.json)
+            manager.save()
         }
     }
 
     private fun backToMainMenu(event: ButtonInteractionEvent) {
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
         event.deferEdit().flatMap {
-            steps[event.user.idLong]?.renderEmbedAction(event.userLocale)
+            step.renderEmbedAction(event.userLocale)
         }.queue()
     }
 
     private fun modifyBtnColor(event: ButtonInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            val idMap = componentIdManager.parse(event.componentId)
-            if (idMap["sub_action"] == "btnColorSubmit") {
-                step.setBtnStyle(ButtonStyle.fromKey(idMap["color_index"] as Int))
-                event.deferEdit().flatMap {
-                    step.renderEmbedAction(event.userLocale)
-                }.queue()
-            }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
+
+        val idMap = componentIdManager.parse(event.componentId)
+        if (idMap["sub_action"] == "btnColorSubmit") {
+            step.setBtnStyle(ButtonStyle.fromKey(idMap["color_index"] as Int))
+            event.deferEdit().flatMap {
+                step.renderEmbedAction(event.userLocale)
+            }.queue()
         }
     }
 
 
     /* ------------------------------------- */
     fun onEntitySelectInteraction(event: EntitySelectInteractionEvent) {
-        steps[event.user.idLong]?.let { step ->
-            val idMap = componentIdManager.parse(event.componentId)
-            when (idMap["sub_action"]) {
-                "admin" -> {
-                    step.setAdminIds(event.values.map { it.idLong })
-                }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
 
-                "category" -> {
-                    val categoryId = event.values[0].idLong
-                    if (categoryId != 0L && event.guild!!.getCategoryById(categoryId) == null) {
-                        event.reply("Cannot find a category with the id ${categoryId}!").setEphemeral(true).queue()
-                        return
-                    }
-
-                    step.setCategoryId(categoryId)
-                }
+        val idMap = componentIdManager.parse(event.componentId)
+        when (idMap["sub_action"]) {
+            "admin" -> {
+                step.setAdminIds(event.values.map { it.idLong })
             }
 
-            event.deferEdit().flatMap {
-                step.renderEmbedAction(event.userLocale)
-            }.queue()
+            "category" -> {
+                val categoryId = event.values[0].idLong
+                if (categoryId != 0L && event.guild!!.getCategoryById(categoryId) == null) {
+                    event.reply("Cannot find a category with the id ${categoryId}!").setEphemeral(true).queue()
+                    return
+                }
+
+                step.setCategoryId(categoryId)
+            }
         }
+
+        event.deferEdit().flatMap {
+            step.renderEmbedAction(event.userLocale)
+        }.queue()
     }
 
 
     /* ------------------------------------- */
     fun onModalInteraction(event: ModalInteractionEvent, idMap: Map<String, Any>) {
-        steps[event.user.idLong]?.let { step ->
-            when (idMap["sub_action"]) {
-                "author" -> {
-                    step.setAuthor(event.getValue("author")?.asString, event.getValue("image")?.asString)
-                }
+        val step = steps[event.user.idLong] ?: return event.deferEdit().queue()
 
-                "content" -> {
-                    step.setTitle(event.getValue("title")?.asString)
-                    step.setDesc(event.getValue("desc")?.asString)
-                }
-
-                "reason" -> {
-                    step.setReason(event.getValue("reason")!!.asString)
-                }
-
-                "color" -> {
-                    step.setColor(
-                        Integer.parseInt(event.getValue("color")!!.asString.substring(1), 16)
-                    )
-                }
-
-                "btnContent" -> {
-                    val btnText = event.getValue("btnText")
-                    val btnEmoji = event.getValue("btnEmoji")
-
-                    if (btnText == null && btnEmoji == null) {
-                        event.reply("Either ButtonText or ButtonEmoji must be provided!").setEphemeral(true).queue()
-                        return
-                    }
-
-                    step.setBtnContent(btnText?.asString)
-                    step.setBtnEmoji(
-                        btnEmoji?.let {
-                            if (it.asString.isNotEmpty()) Emoji.fromUnicode(it.asString)
-                            else null
-                        }
-                    )
-                }
+        when (idMap["sub_action"]) {
+            "author" -> {
+                step.setAuthor(event.getValue("author")?.asString, event.getValue("image")?.asString)
             }
 
-            event.deferEdit().flatMap {
-                step.renderEmbedAction(event.userLocale)
-            }.queue()
+            "content" -> {
+                step.setTitle(event.getValue("title")?.asString)
+                step.setDesc(event.getValue("desc")?.asString)
+            }
+
+            "reason" -> {
+                step.setReason(event.getValue("reason")!!.asString)
+            }
+
+            "color" -> {
+                step.setColor(
+                    Integer.parseInt(event.getValue("color")!!.asString.substring(1), 16)
+                )
+            }
+
+            "btnContent" -> {
+                val btnText = event.getValue("btnText")
+                val btnEmoji = event.getValue("btnEmoji")
+
+                if (btnText == null && btnEmoji == null) {
+                    event.reply("Either ButtonText or ButtonEmoji must be provided!").setEphemeral(true).queue()
+                    return
+                }
+
+                step.setBtnContent(btnText?.asString)
+                step.setBtnEmoji(
+                    btnEmoji?.let {
+                        if (it.asString.isNotEmpty()) Emoji.fromUnicode(it.asString)
+                        else null
+                    }
+                )
+            }
         }
+
+        event.deferEdit().flatMap {
+            step.renderEmbedAction(event.userLocale)
+        }.queue()
     }
 
 
