d3d0c0c0d111534a98c26db7761804eab34b355e
xinshoutw <me@xinshou.tw>
2026-01-21 17:01:17 +0800

    chore: add Maven Central publishing with signing configuration
    - Implemented Maven publishing setup using `com.vanniktech.maven.publish`.
    - Configured GPG signing for all publications.
    - Introduced POM metadata with project details, licenses, developers, and SCM information.
    - Enabled `sourcesJar` generation in Java plugin setup.

--- Core/build.gradle.kts
+++ Core/build.gradle.kts
@@ -5,9 +5,55 @@ plugins {
     kotlin("plugin.serialization")
     id("com.gradleup.shadow")
     id("xs-jvm-conventions")
+    id("com.vanniktech.maven.publish") version "0.30.0"
+    id("signing")
 }
 
 repositories { mavenCentral() }
+
+
+java {
+    withSourcesJar()
+}
+
+signing {
+    useGpgCmd()
+}
+
+mavenPublishing {
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates(group.toString(), "discord-bot-core", version.toString())
+
+    pom {
+        name.set("discord-bot-core")
+        description.set("This is a discord bot core. You can add plugins in your own!")
+        inceptionYear.set("2026")
+        url.set("https://github.com/xinshoutw/XsDiscordBotKt")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("repo")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("xinshoutw")
+                name.set("xinshoutw")
+                url.set("https://github.com/xinshoutw/")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/xinshoutw/XsDiscordBotKt")
+            connection.set("scm:git:git://github.com/xinshoutw/XsDiscordBotKt.git")
+            developerConnection.set("scm:git:ssh://git@github.com/xinshoutw/XsDiscordBotKt.git")
+        }
+    }
+}
 
 dependencies {
     compileOnly(libs.annotations)
@@ -20,13 +66,20 @@ dependencies {
     api(libs.moshi.kotlin)
     api(libs.kotlinx.coroutines.core)
 
-    implementation(libs.embed.mongo) // Embedded MongoDb
-    implementation(libs.protobuf.java) // CVE fix
-    implementation(libs.jline) // CLI
-    implementation(libs.clikt) // Run Arg
-    implementation(libs.jansi) // AnsiConsole
-    implementation(libs.jsoup) // Connection
+    implementation(libs.embed.mongo)
+    implementation(libs.protobuf.java)
+    implementation(libs.jline)
+    implementation(libs.clikt)
+    implementation(libs.jansi)
+    implementation(libs.jsoup)
     implementation(libs.kotlin.reflect)
+}
+
+try {
+    tasks.named("sourcesJar") {
+        dependsOn("generateVersion")
+    }
+} catch (e: Exception) { /* ignore */
 }
 
 tasks.named<ShadowJar>("shadowJar") {
